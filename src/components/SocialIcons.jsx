b49e19ba2b975e4f259a3f241743fd5895354d7d
Basit Warsi <basitwarsi2595@gmail.com>
2025-06-17 17:13:54 +0530

    fixes

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import "./SocialIcons.css";
-
+import './SocialIcons.css'
 export default function SocialIcons() {
   return (
-    <div className="social-icons">
-      <a href="https://instagram.com" target="_blank">
+    <div className="social-icons1">
+      <a href="https://www.instagram.com/sangam.iitjmu?igsh=dXZ3azVuaXdnb3Ey" target="_blank">
         <img src="/icons/instagram.png" alt="Instagram" />
       </a>
-      <a href="mailto:someone@example.com">
+      <a href="https://www.linkedin.com/in/sangam-iit-jammu-a36244354?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
+        <img src="/icons/linkdin.png" alt="Instagram" id="invert"/>
+      </a>
+      <a href="mailto:sangam@iitjammu.ac.in">
         <img src="/icons/mail.png" alt="Email" />
       </a>
     </div>
