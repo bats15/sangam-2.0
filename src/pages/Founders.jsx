c6b4319e16f6026a0b458af6ccb89be8cbdec254
Basit Warsi <basitwarsi2595@gmail.com>
2025-06-18 12:24:23 +0530

    .

--- src/pages/Founders.jsx
+++ src/pages/Founders.jsx
@@ -4,21 +4,21 @@ export default function Founders(){
     const founders = [
     {
       "name": "Kanishka Chholak",
-      "role": "Senior Advisor",
+      "role": "Cheif Editor",
       "id": "2023uce0052",
       "img" : "",
       "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nostrum illum quasi eligendi aut. Tempora eveniet quidem fuga iusto deserunt laudantium, eius placeat necessitatibus temporibus quaerat officia. Sint cupiditate quidem ipsam, tenetur exercitationem enim, assumenda perspiciatis ad saepe, explicabo eius sequi alias. Tempore sapiente repellat eaque corporis eius, dolorem earum minima nihil. Cum vel delectus repudiandae!",
     },
     {
       "name": "Akshay Malwey",
-      "role": "Senior Advisor",
+      "role": "Head of Content",
       "id": "2023ucs0080",
       "img" : "",
       "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nostrum illum quasi eligendi aut. Tempora eveniet quidem fuga iusto deserunt laudantium, eius placeat necessitatibus temporibus quaerat officia. Sint cupiditate quidem ipsam, tenetur exercitationem enim, assumenda perspiciatis ad saepe, explicabo eius sequi alias. Tempore sapiente repellat eaque corporis eius, dolorem earum minima nihil. Cum vel delectus repudiandae!"
     },
     {
-      "name": "Bhoomi Yadav",
-      "role": "Senior Advisor",
+      "name": "Rohit Khatri",
+      "role": "Head of Designing",
       "id": "2023uch0012",
       "img" : "",
       "para": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nostrum illum quasi eligendi aut. Tempora eveniet quidem fuga iusto deserunt laudantium, eius placeat necessitatibus temporibus quaerat officia. Sint cupiditate quidem ipsam, tenetur exercitationem enim, assumenda perspiciatis ad saepe, explicabo eius sequi alias. Tempore sapiente repellat eaque corporis eius, dolorem earum minima nihil. Cum vel delectus repudiandae!"
