bc11b075559545c9f8ea45b9bd10ac3707edf884
Basit Warsi <basitwarsi2595@gmail.com>
2025-06-19 11:15:15 +0530

    trial-code

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,11 +1,19 @@
 import Page8 from "./Page8";
 import Founders from './Founders'
+import './Team.css'
+import { useState } from "react";
 
-export default function Team(){
-    return(
-        <>
-        <Page8/>
-        <Founders/>
-        </>
-    )
+export default function Team() {
+    const [teamVisible, setTeamVisible] = useState(true);
+
+    return (
+        <main className="team-founder-container">
+            <div className="page-buttons">
+                <button onClick={() => setTeamVisible(true)}>Team</button>
+                <button onClick={() => setTeamVisible(false)}>Founders</button>
+            </div>
+
+            {teamVisible ? <Page8 /> : <Founders />}
+        </main>
+    );
 }
